e97831c44966f58cc675c111f86c88bd9cfa7d8d
k6ymaker <k6ymaker@gmail.com>
2021-09-25 21:58:02 +0800

    readobject

---
+++ readobject/src/main/java/com/test/MyObject.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+class MyObject implements Serializable {
+    public String name;
+    //重写readObject()方法
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
+        //执行默认的readObject()方法
+        in.defaultReadObject();
+        //执行打开计算器程序命令
+        Runtime.getRuntime().exec("open /System/Applications/Calculator.app");
+    }
+}
